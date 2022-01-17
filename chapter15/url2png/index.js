518b5bfbc9a86adfe21192d0f2f0488dcba3c456
Andreas Wittig <andreas@widdix.de>
2022-01-17 10:38:04 +0100

    Updating examples

--- chapter15/url2png/index.js
+++ chapter15/url2png/index.js
@@ -1,16 +1,14 @@
 const AWS = require('aws-sdk');
-const uuid = require('uuid/v4');
+var { v4: uuidv4 }  = require('uuid');
 const config = require('./config.json');
-const sqs = new AWS.SQS({
-  region: 'us-east-1'
-});
+const sqs = new AWS.SQS({});
 
 if (process.argv.length !== 3) {
   console.log('URL missing');
   process.exit(1);
 }
 
-const id = uuid();
+const id = uuidv4();
 const body = {
   id: id,
   url: process.argv[2]
@@ -23,6 +21,6 @@ sqs.sendMessage({
   if (err) {
     console.log('error', err);
   } else {
-    console.log('PNG will be soon available at http://' + config.Bucket + '.s3-website-us-east-1.amazonaws.com/' + id + '.png');
+    console.log('PNG will be soon available at http://' + config.Bucket + '.s3.amazonaws.com/' + id + '.png');
   }
 });
