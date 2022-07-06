3c9ca2b34a2497e0653eeb7997aca1a992fd43fc
Andreas Wittig <andreas@widdix.de>
2022-07-06 11:01:42 +0200

    Updating ch08

--- chapter08/gallery/server.js
+++ chapter08/gallery/server.js
@@ -1,74 +1,72 @@
-var express = require('express');
-var AWS = require('aws-sdk');
-var mu = require('mu2-updated');
-var uuid = require('uuid');
-var multiparty = require('multiparty');
+const express = require('express');
+const AWS = require('aws-sdk');
+const mu = require('mu2-updated');
+const uuid = require('uuid');
+const multiparty = require('multiparty');
 
-var app = express();
-var s3 = new AWS.S3({
-	'region': 'us-east-1'
+const app = express();
+const s3 = new AWS.S3({
+  'region': 'us-east-1'
 });
 
-var bucket = process.argv[2];
+const bucket = process.argv[2];
 if (!bucket || bucket.length < 1) {
-	console.error('Missing S3 bucket. Start with node server.js BUCKETNAME instead.');
-	process.exit(1);
+  console.error('Missing S3 bucket. Start with node server.js BUCKETNAME instead.');
+  process.exit(1);
 }
 
-function listImages(response) {
-	var params = {
-		Bucket: bucket
-	};
-	s3.listObjects(params, function(err, data) {
-		if (err) {
-			console.error(err);
-			response.status(500);
-			response.send('Internal server error.');
-		} else {
-			var stream = mu.compileAndRender(
-				'index.html', 
-				{
-					Objects: data.Contents, 
-					Bucket: bucket
-				}
-			);
-			stream.pipe(response);
-		}
-	});
+async function listImages(response) {
+  try {
+    let data = await s3.listObjects({
+      Bucket: bucket
+    }).promise();
+    let stream = mu.compileAndRender(
+      'index.html',
+      {
+        Objects: data.Contents,
+        Bucket: bucket
+      }
+    );
+    stream.pipe(response);
+  } catch (err) {
+    console.error(err);
+    response.status(500);
+    response.send('Internal server error.');
+  }
 }
 
-function uploadImage(image, response) {
-	var params = {
-		Body: image,
-		Bucket: bucket,
-		Key: uuid.v4(),
-		ACL: 'public-read',
-		ContentLength: image.byteCount,
-		ContentType: image.headers['content-type']
-	};
-	s3.putObject(params, function(err, data) {
-		if (err) {
-			console.error(err);
-			response.status(500);
-			response.send('Internal server error.');
-		} else {
-			response.redirect('/');
-		}
-	});
+async function uploadImage(image, response) {
+  try {
+    await s3.putObject({
+      Body: image,
+      Bucket: bucket,
+      Key: uuid.v4(),
+      ACL: 'public-read',
+      ContentLength: image.byteCount,
+      ContentType: image.headers['content-type']
+    }).promise();
+    response.redirect('/');
+  } catch (err) {
+    console.error(err);
+    response.status(500);
+    response.send('Internal server error.');
+  }
 }
 
-app.get('/', function (request, response) {
-	listImages(response);
+app.get('/', async (request, response) => {
+  await listImages(response);
+})
+
+
+
+app.post('/upload', async (request, response) => {
+  let form = new multiparty.Form();
+  form.on('part', async (part) => {
+    await uploadImage(part, response);
+  });
+  form.parse(request);
 });
 
-app.post('/upload', function (request, response) {
-	var form = new multiparty.Form();
-	form.on('part', function(part) {
-		uploadImage(part, response);
-	});
-	form.parse(request);
-});
- 
 app.listen(8080);
 
 console.log('Server started. Open http://localhost:8080 with browser.');
